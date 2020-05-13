cb1c59e1c4fa2fd1be44a80056b3bb4e42ed975e
sukhbinder <ssingh@email.ccom>
2020-05-13 16:33:18 -0300

    Added record name

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
-import json
 from setuptools import find_packages, setup
 
 setup(
@@ -11,7 +10,7 @@ setup(
     author="sukhbinder",
     author_email="sukh2010@yahoo.com",
     entry_points={
-        'console_scripts': ['startrecord = screenrecord.srecord:main']
+        'console_scripts': ['record = screenrecord.srecord:main']
     },
 	install_requires=["imageio", "imageio-ffmpeg"]
 )
